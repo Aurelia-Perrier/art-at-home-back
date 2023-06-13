879672594b72c09467cb886adf0d5abca73440d8
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-06-13 11:04:20 +0200

    use of Route

--- src/Controller/Api/LoginController.php
+++ src/Controller/Api/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LoginController extends AbstractController
