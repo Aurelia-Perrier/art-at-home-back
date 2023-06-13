4d6cb416c1d568964fd216af6edfd759b72eafe1
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-06-13 10:36:45 +0200

    little changing in code

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
-use App\Repository\ArtworkRepository;
 use App\Repository\UserRepository;
 use App\Service\MySlugger;
 use Doctrine\Persistence\ManagerRegistry;
