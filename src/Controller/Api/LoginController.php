0afba8ccf8b632a60824a80437ecd72e8d892cdd
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-06-13 10:43:17 +0200

    WIP - LoginController

---
+++ src/Controller/Api/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginController extends AbstractController
+{
+    /**
+     * Undocumented function
+     *
+     * @Route ("/api/login_check")
+     */
+    public function login()
+    {
+
+    }
+}
